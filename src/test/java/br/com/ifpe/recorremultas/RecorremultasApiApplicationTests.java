1ba9d869cfe7f02679ead828fc91dca2e88a078f
aluno <aluno@ifpe>
2023-05-12 20:21:24 -0300

    criando a API

---
+++ src/test/java/br/com/ifpe/recorremultas/RecorremultasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.recorremultas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecorremultasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
