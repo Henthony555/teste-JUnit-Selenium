b12ac5ac2a3caf2f0386142f3b75bebd52b3402f
Henthony <henthony555@gmail.com>
2023-05-31 15:45:04 -0300

    CRUD Agencias

--- src/main/java/br/com/ifpe/recorremultas/api/agencias/AgenciasController.java
+++ src/main/java/br/com/ifpe/recorremultas/api/agencias/AgenciasController.java
@@ -1,11 +1,17 @@
 package br.com.ifpe.recorremultas.api.agencias;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,14 +24,40 @@ import br.com.ifpe.recorremultas.util.entity.GenericController;
 @RequestMapping("/api/agencias")
 public class AgenciasController extends GenericController {
 
-   @Autowired
-   private AgenciasService agenciasService;
+    @Autowired
+    private AgenciasService agenciasService;
 
-   @PostMapping
-   public ResponseEntity<Agencias> save(@RequestBody @Valid AgenciasRequest request) {
+    @PostMapping
+    public ResponseEntity<Agencias> save(@RequestBody @Valid AgenciasRequest request) {
 
-       Agencias agencias = agenciasService.save(request.build());
-       return new ResponseEntity<Agencias>(agencias, HttpStatus.CREATED);
-   }
+        Agencias agencias = agenciasService.save(request.build());
+        return new ResponseEntity<Agencias>(agencias, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Agencias> listarTodos() {
+
+        return agenciasService.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public Agencias obterPorID(@PathVariable Long id) {
+
+        return agenciasService.obterPorID(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Agencias> update(@PathVariable("id") Long id, @RequestBody AgenciasRequest request) {
+
+        agenciasService.update(id, request.build());
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        agenciasService.delete(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
-
--- src/main/java/br/com/ifpe/recorremultas/modelo/agencias/AgenciasService.java
+++ src/main/java/br/com/ifpe/recorremultas/modelo/agencias/AgenciasService.java
@@ -1,5 +1,7 @@
 package br.com.ifpe.recorremultas.modelo.agencias;
 
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +12,46 @@ import br.com.ifpe.recorremultas.util.entity.GenericService;
 @Service
 public class AgenciasService extends GenericService {
 
-   @Autowired
-   private AgenciasRepository repository;
+    @Autowired
+    private AgenciasRepository repository;
 
-   @Transactional
-   public Agencias save(Agencias agencias) {
+    @Transactional
+    public Agencias save(Agencias agencias) {
 
-       super.preencherCamposAuditoria(agencias);
-       return repository.save(agencias);
-   }
+        super.preencherCamposAuditoria(agencias);
+        return repository.save(agencias);
+    }
+
+    public List<Agencias> listarTodos() {
+
+        return repository.findAll();
+    }
+
+    public Agencias obterPorID(Long id) {
+
+        return repository.findById(id).get();
+    }
+
+    @Transactional
+    public void update(Long id, Agencias agenciasAlterado) {
+
+        Agencias agencias = repository.findById(id).get();
+        agencias.setNomeAgencia(agenciasAlterado.getNomeAgencia());
+        agencias.setLatitude(agenciasAlterado.getLatitude());
+        agencias.setLongitude(agenciasAlterado.getLongitude());
+
+        super.preencherCamposAuditoria(agencias);
+        repository.save(agencias);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        Agencias agencias = repository.findById(id).get();
+        agencias.setHabilitado(Boolean.FALSE);
+        super.preencherCamposAuditoria(agencias);
+
+        repository.save(agencias);
+    }
 
 }
-
