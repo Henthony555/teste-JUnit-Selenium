b33c4988d7fd8a72697994a1eaa002d5c724e34b
aluno <aluno@gmail.com>
2023-06-06 19:12:06 -0300

    Usuario atualizada

--- src/main/java/br/com/ifpe/recorremultas/modelo/usuario/UsuarioService.java
+++ src/main/java/br/com/ifpe/recorremultas/modelo/usuario/UsuarioService.java
@@ -32,4 +32,29 @@ public class UsuarioService extends GenericService {
      return repository.findById(id).get();
  }
 
+ @Transactional
+    public void update(Long id, Usuario usuarioAlterado) {
+
+        Usuario usuario = repository.findById(id).get();
+        usuario.setNomeCompleto(usuarioAlterado.getNomeCompleto());
+        usuario.setDataNascimento(usuarioAlterado.getDataNascimento());
+        usuario.setCpf(usuarioAlterado.getCpf());
+        usuario.setEmail(usuarioAlterado.getEmail());
+        usuario.setSenha(usuarioAlterado.getSenha());
+     
+        super.preencherCamposAuditoria(usuario);
+        repository.save(usuario);
+    }
+
+    @Transactional
+    public void delete(Long id) {
+
+        Usuario usuario = repository.findById(id).get();
+        usuario.setHabilitado(Boolean.FALSE);
+        super.preencherCamposAuditoria(usuario);
+
+        repository.save(usuario);
+    }
+
 }
+
